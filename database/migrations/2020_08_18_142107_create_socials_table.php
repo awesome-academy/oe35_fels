6b9bdd4d3f0ae414bdd786624b11368f59fa5c75
phuongsai <phuonghoang.s21@gmail.com>
2020-08-19 10:07:50 +0700

    setup database migration

---
+++ database/migrations/2020_08_18_142107_create_socials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('provider_id')->unique()->nullable();
+            $table->string('provider_name')->nullable();
+            $table->string('access_token')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socials');
+    }
+}
